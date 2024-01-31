5a35f158ba84ca2b181ffc0457399b23fac16545
David Stevens <mail.davro@gmail.com>
2024-01-31 09:54:40 +0000

    index, index.js, site json, data images update

--- assets/index.js
+++ assets/index.js
@@ -54,24 +54,25 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error("Error fetching JSON:", error));
 
-    // Function to create an article card
-    function createArticleCardOLD(article) {
-        const card = document.createElement("div");
-        card.className = "col";
+    //
+    // Function to format the date string
+    //
+    function formatDate(dateString) {
+        if (!dateString) {
+            return ''; // Return empty string if date is not provided
+        }
 
-        //<p class="card-text">${article.content}</p>
-            card.innerHTML = `
-            <div class="card">
-            <div class="card-body">
-            <h5 class="card-title">${article.title}</h5>
-            <a href="article/${article.date}/${article.url}"><img src="data/${article.date}/${article.image}-300x157.jpg" /></a>
-            </div>
-            </div>
-            `;
+        const date = new Date(dateString);
+        if (isNaN(date.getTime())) {
+            return ''; // Return empty string if the date is invalid
+        }
 
-        return card;
+        const year = date.getFullYear();
+        const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Month is zero-based
+        return `${year}/${month}`;
     }
 
+    //
     // Function to create an article card
     //
     // images
@@ -79,61 +80,42 @@ document.addEventListener("DOMContentLoaded", function () {
     //   -300x157.jpg
     //   -768x402.jpg
     //
-    function createArticleCardTEST(article) {
-        const card = document.createElement("div");
-        card.className = "col-md-4"; // Set the column size for medium devices (desktop)
-        card.innerHTML = `
-            <div class="card">
-            <a href="article/${article.url}">
-            <img srcset="data/${article.date}/${article.image}-150x150.jpg 150w, data/${article.date}/${article.image}-300x157.jpg 300w, data/${article.date}/${article.image}-768x402.jpg 768w"
-                 sizes="(max-width: 576px) 150px, (max-width: 768px) 300px, 768px"
-                 src="data/${article.date}/${article.image}-300x157.jpg"
-                 alt="${article.title} Image"
-                 class="card-img-top" />
-            </a>
-            <div class="card-body">
-            <h5 class="card-title">${article.title}</h5>
-            </div>
-            </div>
-            `;
-        return card;
-    }
-
-
-    // Function to format the date string
-    function formatDate(dateString) {
-        const date = new Date(dateString);
-        const year = date.getFullYear();
-        const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Month is zero-based
-        return `${year}/${month}`;
-    }
-
-    // Function to create an article card
     function createArticleCard(article) {
         const formattedDate = formatDate(article.date);
 
         const card = document.createElement("div");
         card.className = "col-md-4"; // Set the column size for medium devices (desktop)
+
+        let AImgTag = ''; // Initialize an empty string for a - img tag
+
+        // Check if article.image is provided
+        if (article.image) {
+            AImgTag = `
+                <a href="article/${formattedDate}/${article.url}">
+                <img srcset="data/${formattedDate}/${article.image}-150x150.jpg 150w,
+                data/${formattedDate}/${article.image}-300x157.jpg 300w,
+                data/${formattedDate}/${article.image}-768x402.jpg 768w"
+            sizes="(max-width: 576px) 150px,
+                (max-width: 768px) 300px,
+                768px"
+            src="data/${formattedDate}/${article.image}-300x157.jpg"
+            alt="${article.title} Image"
+            class="card-img-top">
+                </a>
+                `;
+        }
+
+        // Create the Card HTML
         card.innerHTML = `
             <div class="card">
-            <a href="article/${formattedDate}/${article.url}"> <!-- Replace ${article.date} with formattedDate -->
-            <img srcset="data/${article.date}/${article.image}-150x150.jpg 150w,
-            data/${article.date}/${article.image}-300x157.jpg 300w,
-            data/${article.date}/${article.image}-768x402.jpg 768w"
-        sizes="(max-width: 576px) 150px,
-            (max-width: 768px) 300px,
-            768px"
-        src="data/${article.date}/${article.image}-300x157.jpg"
-        alt="${article.title} Image"
-        class="card-img-top">
-            </a>
+            ${AImgTag}
             <div class="card-body">
             <h5 class="card-title">${article.title}</h5>
             </div>
             </div>
             `;
+
         return card;
     }
 
 });
-
